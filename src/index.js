563259a43a990519645ec64e2e4579d0ed62b6d7
AVVS <v@makeomatic.ca>
2024-01-20 09:10:50 -0800

    feat: upgrade to ensure microfleet/validation is of compatible version

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-const assert = require('assert');
+const assert = require('node:assert/strict');
 const merge = require('lodash.merge');
 const Validator = require('@microfleet/validation').default;
 
